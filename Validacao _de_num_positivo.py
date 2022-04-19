b931507eab1bda6cb49220f528b62d42f3169b03
Guilherme Reis <96945852+GuilhermeReisbr@users.noreply.github.com>
2022-04-19 05:42:27 -0300

    Create Validacao _de_num_positivo.py

---
+++ Validacao _de_num_positivo.py
@@ -0,0 +1,10 @@
+'''
+Faça um prograa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+'''
+
+num = int(input('Digite um numero: '))
+
+if num < 0:
+    print(num,'é negativo')
+else:
+    print(num,'é positivo')
